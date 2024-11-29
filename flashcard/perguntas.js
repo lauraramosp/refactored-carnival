41e3b7da6708d9b55e95bf88e499a44e92dd2262
lauraramosp <lauraramospauro9@gmail.com>
2024-11-29 18:53:09 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Quanto que é 5!?',
+    '5.4.3.2.1=120'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a formula da area de um quadrado?',
+    'base x altura '
+)
+
+criaCartao(
+    'Programação',
+    'Qual o ano que começou a Segunda guerra mundial?',
+    'ocorreu dia 1 de setembro de 1939'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
